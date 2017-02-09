1eacd1d585947800427d6c767892f043c08b03fb
Aman Thakral <a@thakral.in>
2017-02-09 11:41:35 -0600

    fixed deployment script

--- deploy/deploySeq.sh
+++ deploy/deploySeq.sh
@@ -5,10 +5,14 @@ git checkout .;
 # this is using ssh key of user 
 # username: deploy_readonly_user, password : Deploy-1232
 git pull origin release 
-cd src/PowerNetwork.Web
 sudo systemctl stop kestrel-powernetwork-ui.service
+cd src/PowerNetwork.Core
+dotnet restore
+dotnet build
+cd ../PowerNetwork.Web
+dotnet restore
+dotnet build
 dotnet publish --configuration release
 sudo systemctl start kestrel-powernetwork-ui.service
-
 echo "---------------------------------Deployment End ------------------------------------------------------"
 
