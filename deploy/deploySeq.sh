3dfac4634de0c41b17a3b6a57eaa14254330c988
Aman Thakral <a@thakral.in>
2017-02-09 11:26:55 -0600

    updated the script to only consider release

--- deploy/deploySeq.sh
+++ deploy/deploySeq.sh
@@ -1,9 +1,14 @@
+echo "---------------------------------Deployment Starting ------------------------------------------------------"
+date
 cd ~/PowerNetwork
 git checkout .; 
 # this is using ssh key of user 
 # username: deploy_readonly_user, password : Deploy-1232
-git pull origin master
+git pull origin release 
 cd src/PowerNetwork.Web
 sudo systemctl stop kestrel-powernetwork-ui.service
 dotnet publish --configuration release
 sudo systemctl start kestrel-powernetwork-ui.service
+
+echo "---------------------------------Deployment End ------------------------------------------------------"
+
